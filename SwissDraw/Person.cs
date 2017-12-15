b7a6b4310413359432f87983865b0ebfa7dcdb6c
yoshzawa <yoshzawa@users.noreply.github.com>
2017-12-15 09:53:40 +0900

    Update Person.cs

--- SwissDraw/Person.cs
+++ SwissDraw/Person.cs
@@ -8,8 +8,11 @@ namespace SwissDraw
 {
     public class Person
     {
+        // くじの番号
         public int LotNumber { get; set; }
+        // グループ名（1文字のアルファベット）
         public string PersonGroup { get; set; }
+        // 参加者名
         public string PersonName { get; set; }
     }
 
