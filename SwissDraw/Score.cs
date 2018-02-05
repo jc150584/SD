ece1b984cf88fceaf888ac04c484490c03517a09
有希奈 澤田 <150584@jc-21.jp>
2018-02-05 17:21:18 +0900

    scoreできた

---
+++ SwissDraw/Score.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwissDraw
+{
+    public class Score
+    {
+        // くじの番号
+        public int LotNumber { get; set; }
+        public int winCount { get; set; }
+        public int score { get; set; }
+
+        //コンストラクタ
+        public Score(int no, int count, int s)
+        {
+            LotNumber = no;
+            winCount = count;
+            score = s;
+        }
+
+        public static Dictionary<int, Score> CalcWinClont(Match[] result)
+        {
+            
+            Dictionary<int, Score> s = new Dictionary<int, Score>();
+            int wincount;
+            int[] key = new int[100];
+            foreach (Match m in result)
+            {
+                foreach(int k in key)
+                {
+                     wincount = Match.GetWinCount(m.Person1, result);
+                     s[m.Person1] = new Score(m.Person1, wincount, 0);
+
+                     wincount = Match.GetWinCount(m.Person2, result);
+                     s[m.Person2] = new Score(m.Person2, wincount, 0);
+                }
+            }
+            return s;
+        }
+
+        public static Dictionary<int, Score> CalcScore(Match[] result)
+        {
+            Dictionary<int, Score> c = CalcWinClont(result);
+
+            foreach (Match m in result)
+            {
+                //person1が勝ったら1、person2が勝ったら2
+                int i = m.Result;
+                Score s1 = c[m.Person1];
+                Score s2 = c[m.Person2];   
+                
+                //person1が勝ったとき、person2の勝ち数分スコアを増やす
+                if (i == 1)
+                {
+                    int score = s1.score;
+                    int plus = s2.winCount;
+                    int newscore = score + plus;
+                    c[m.Person1] = new Score(m.Person1, s1.winCount, newscore);
+                }
+                else if (i == 2)
+                {
+                    int score = s2.score;
+                    int plus = s1.winCount;
+                    int newscore = score + plus;
+                    c[m.Person2] = new Score(m.Person2, s2.winCount, newscore);
+                }
+            }
+            return c;
+        }
+    }
+}
---
+++ SwissDrawTests/ScoreTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SwissDraw;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwissDraw.Tests
+{
+    [TestClass()]
+    public class ScoreTests
+    {
+        [TestMethod()]
+        public void CalcWinClontTest()
+        {
+            Match[] result = new Match[3];
+            result[0] = new Match(1, 3);
+            result[0].Result = 1;
+            result[1] = new Match(2, 5);
+            result[1].Result = 1;
+            result[2] = new Match(4, 6);
+            result[2].Result = 1;
+
+            var score = Score.CalcScore(result);
+            Assert.AreEqual(1, score[1].winCount);
+            Assert.AreEqual(1, score[2].winCount);
+            Assert.AreEqual(1, score[4].winCount);
+            Assert.AreEqual(0, score[3].winCount);
+            Assert.AreEqual(0, score[5].winCount);
+            Assert.AreEqual(0, score[6].winCount);
+
+            Match[] result2 = new Match[6];
+            result2[0] = new Match(1, 3);
+            result2[0].Result = 1;
+            result2[1] = new Match(2, 5);
+            result2[1].Result = 1;
+            result2[2] = new Match(4, 6);
+            result2[2].Result = 1;
+            result2[3] = new Match(1, 2);
+            result2[3].Result = 1;
+            result2[4] = new Match(4, 5);
+            result2[4].Result = 1;
+            result2[5] = new Match(3, 6);
+            result2[5].Result = 1;
+
+            var score2 = Score.CalcScore(result2);
+            Assert.AreEqual(2, score2[1].winCount);
+            Assert.AreEqual(1, score2[2].winCount);
+            Assert.AreEqual(1, score2[3].winCount);
+            Assert.AreEqual(2, score2[4].winCount);
+            Assert.AreEqual(0, score2[5].winCount);
+            Assert.AreEqual(0, score2[6].winCount);
+
+
+        }
+
+        [TestMethod()]
+        public void CalcScoreTest()
+        {
+            Match[] result2 = new Match[6];
+            result2[0] = new Match(1, 3);
+            result2[0].Result = 1;
+            result2[1] = new Match(2, 5);
+            result2[1].Result = 1;
+            result2[2] = new Match(4, 6);
+            result2[2].Result = 1;
+            result2[3] = new Match(1, 2);
+            result2[3].Result = 1;
+            result2[4] = new Match(4, 5);
+            result2[4].Result = 1;
+            result2[5] = new Match(3, 6);
+            result2[5].Result = 1;
+
+            var score2 = Score.CalcScore(result2);
+            Assert.AreEqual(2, score2[1].winCount);
+            Assert.AreEqual(1, score2[2].winCount);
+            Assert.AreEqual(1, score2[3].winCount);
+            Assert.AreEqual(2, score2[4].winCount);
+            Assert.AreEqual(0, score2[5].winCount);
+            Assert.AreEqual(0, score2[6].winCount);
+
+            Assert.AreEqual(1 + 1, score2[1].score);    //3 , 2
+            Assert.AreEqual(0, score2[2].score);        //5
+            Assert.AreEqual(0, score2[3].score);        //6
+            Assert.AreEqual(0 + 0, score2[4].score);    //5 , 6
+            Assert.AreEqual(0, score2[5].score);
+            Assert.AreEqual(0, score2[6].score);
+        }
+    }
+}
